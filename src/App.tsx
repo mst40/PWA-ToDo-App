e481adace7e1729b428b110ddc0622c95de31421
mst40 <35ma.ma.ma19@gmail.com>
2024-01-11 18:42:03 +0900

    update filter, remove, clear trash can

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,32 @@ type Todo = {
   value: string;
   readonly id: number;
   checked: boolean;
+  removed: boolean;
 }
+
+type Filter = "all" | "checked" | "current" | 'removed'
 
 export const App = () => {
   const [text, setText] = useState<string>('');
   const [todos, setTodos] = useState<Todo[]>([])
+  const [filter, setFilter] = useState<Filter>('all')
+
+  const filteredTodos: Todo[] = todos.filter(todo => {
+    switch (filter) {
+      case 'all': {
+        return !todo.removed;
+      }
+      case 'checked': {
+        return !todo.removed && todo.checked
+      }
+      case 'current': {
+        return !todo.removed && !todo.checked
+      }
+      case 'removed': {
+        return todo.removed;
+      }
+    }
+  })
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value)
@@ -22,6 +43,7 @@ export const App = () => {
       value: text,
       id: new Date().getTime(),
       checked: false,
+      removed: false,
     }
 
     setTodos(prev => [newTodo, ...prev])
@@ -32,13 +54,10 @@ export const App = () => {
     setTodos(todos => {
       const newTodos: Todo[] = todos.map(todo => {
         if (todo.id === id) {
-          //{id:todo.id, value:newValue}
           return { ...todo, value: newValue }
         }
         return todo
       })
-      // console.log('original')
-      // todos.map(todo => { console.log(`id: ${todo.id} value:${todo.value}`) })
       return newTodos
     })
   }
@@ -55,34 +74,84 @@ export const App = () => {
     })
   }
 
+  const handleRemove = (id: number, removed: boolean) => {
+    setTodos(todos => {
+      const newTodos: Todo[] = todos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, removed }
+        }
+        return todo
+      })
+      return newTodos
+    })
+  }
+
+  const handleFilter = (filter: Filter) => {
+    setFilter(filter)
+  }
+
+  const handleEmpty = () => {
+    setTodos(todos => todos.filter(todo => !todo.removed))
+  }
+
+
+
   return (
     <>
-      <form
-        onSubmit={e => {
-          e.preventDefault()
-          handleSubmit()
-        }}>
-        <input type='text'
-          value={text}
-          onChange={e => handleChange(e)} />
-        <input type='submit' value={'ADD'} />
-      </form>
+      <select
+        defaultValue={'all'}
+        onChange={e => handleFilter(e.target.value as Filter)}
+      >
+        <option value="all">All</option>
+        <option value="checked">Done</option>
+        <option value="current">Current</option>
+        <option value="removed">Trash Can</option>
+      </select>
+
+      {filter === 'removed' ? (
+        <button
+          onClick={handleEmpty}
+          disabled={todos.filter(todo => todo.removed).length === 0}
+        >
+          Clear Trash Can
+        </button>
+      ) : (
+        filter !== 'checked' && (
+          <form onSubmit={e => {
+            e.preventDefault()
+            handleSubmit()
+          }}>
+
+            <input type='text' value={text} onChange={e => handleChange(e)} />
+
+            <input type='submit' value={'ADD'} />
+          </form>))}
+
+
       <ul>
-        {todos.map(todo => {
+        {filteredTodos.map(todo => {
           return (
             <li key={todo.id}>
               <input
                 type='checkbox'
+                disabled={todo.removed}
                 checked={todo.checked}
                 //checkフラグを反転
                 onChange={() => handleCheck(todo.id, !todo.checked)}
               />
+
               <input
                 type='text'
-                disabled={todo.checked}
+                disabled={todo.checked || todo.removed}
                 value={todo.value}
                 onChange={e => handleEdit(todo.id, e.target.value)}
               />
+
+              <button
+                onClick={() => handleRemove(todo.id, !todo.removed)}
+              >
+                {todo.removed ? 'Restore' : 'Delete'}
+              </button>
             </li>)
         })}
       </ul>

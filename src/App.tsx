88abd6ef424c5b3dc258c8a96581938a7053779b
mst40 <35ma.ma.ma19@gmail.com>
2024-01-11 17:34:04 +0900

    add Input form & todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,70 @@
+import React, { useState } from 'react'
 import './App.css'
 
-export  const App = () => {
+type Todo = {
+  value: string;
+  readonly id: number;
+}
+
+export const App = () => {
+  const [text, setText] = useState<string>('');
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    if (!text) { return; }
+
+    const newTodo: Todo = {
+      value: text,
+      id: new Date().getTime(),
+    }
+
+    setTodos(prev => [newTodo, ...prev])
+    setText('')
+  }
+
+  const handleEdit = (id: number, newValue: string) => {
+    setTodos(todos => {
+      const newTodos: Todo[] = todos.map(todo => {
+        if (todo.id === id) {
+          //{id:todo.id, value:newValue}
+          return { ...todo, value: newValue }
+        }
+        return todo
+      })
+      console.log('original')
+      todos.map(todo => { console.log(`id: ${todo.id} value:${todo.value}`) })
+      return newTodos
+    })
+  }
 
   return (
     <>
-      <form onSubmit={e => e.preventDefault()}>
-        <input type='text' value="" onChange={e => e.preventDefault()} />
-        <input type='submit' value={'ADD'} onSubmit={e => e.preventDefault()} />
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          handleSubmit()
+        }}>
+        <input type='text'
+          value={text}
+          onChange={e => handleChange(e)} />
+        <input type='submit' value={'ADD'} />
       </form>
+      <ul>
+        {todos.map(todo => {
+          return (
+            <li key={todo.id}>
+              <input
+                type='text'
+                value={todo.value}
+                onChange={e => handleEdit(todo.id, e.target.value)}
+              />
+            </li>)
+        })}
+      </ul>
     </>
   )
 }
